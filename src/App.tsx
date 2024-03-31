6f4cc95647e654fb085531b6cd9d38ac45517cff
JinggaJilca <jinggajilca@gmail.com>
2024-03-31 20:20:40 +0700

    add about pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/nav'
 import Main from './components/main'
+import About from './pages/about'
 
 function App() {
   return (
     <>
     <Nav/>
     <Main/>
+    <About/>
     </>
   )
 }
---
+++ src/pages/about.jsx
@@ -0,0 +1,6 @@
+const about =() => {
+    return(
+        <h1>About Pages</h1>
+    )
+}
+export default about
