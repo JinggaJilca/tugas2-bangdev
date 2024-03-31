965f7f5f44f83c1153f22d9f748f0a1bb1dbfbe7
JinggaJilca <jinggajilca@gmail.com>
2024-03-31 22:28:41 +0700

    all pages ready

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,21 +1,18 @@
+import { useNavigate } from "react-router-dom"
 const nav =() => {
+    const navigate = useNavigate()
     return(
         <>
             <div className="flex items-center justify-between p-10 lg:flex-row border-b bg-white">
                 <div className="font-bold text-3xl tracking-wider flex items-center text-green-800">FINESSE FASHION</div>
 
                 <div className="space-x-4">
-                    <a href="#" className="hover:text-green-800">Home</a>
-                    <a href="#" className="hover:text-green-800">About Us</a>
-                    <a href="#" className="hover:text-green-800">Our Product</a>
-                    <a href="#" className="hover:text-green-800">Contact Us</a>
+                    <a onClick={() => navigate('/')} className="hover:text-green-800 cursor-pointer">Home</a>
+                    <a onClick={() => navigate('/about')}className="hover:text-green-800 cursor-pointer">About</a>
+                    <a onClick={() => navigate('/product')}className="hover:text-green-800 cursor-pointer">Product</a>
+                    <a onClick={() => navigate('/contact')}className="hover:text-green-800 cursor-pointer">Contact Us</a>
                 </div>
-            </div>
-
-            
-                
-            
-            
+            </div> 
         </>
     )
 }
